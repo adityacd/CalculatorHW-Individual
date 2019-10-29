75f7c773a13fd7873e4edf818a835da205a7c29c
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2019-10-29 15:53:53 -0400

    Feature: Setup the CalculatorTests.py to instantiate the Calculator.

---
+++ src/CalculatorTests.py
@@ -0,0 +1,16 @@
+import unittest
+
+from Calculator import Calculator
+
+
+class MyTestCase(unittest.TestCase):  # Accessing the methods
+
+    def setUp(self) -> None:
+        self.calobject = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calobject, Calculator)
+
+
+if __name__ == '__main__':  # This is just causing the unitttest to run
+    unittest.main()
