a0836ae90235712021ee6e96745df7d474186aba
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2019-10-29 16:21:05 -0400

    Feature: Created the test to check the addition.

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -2,6 +2,7 @@ import unittest
 
 from Calculator import Calculator
 
+from CsvReading import CsvReader
 
 class MyTestCase(unittest.TestCase):  # Accessing the methods
 
@@ -10,6 +11,12 @@ class MyTestCase(unittest.TestCase):  # Accessing the methods
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calobject, Calculator)
+
+    def test_add_method_calculator(self):
+        test_data = CsvReader('./src/csvfiles/Unit Test Addition.csv').data
+        for row in test_data:
+            self.assertEqual(self.calobject.add(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calobject.result, int(row['Result']))
 
 
 
