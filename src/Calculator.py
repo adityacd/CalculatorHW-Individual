66a2d6ea9e71f1a4e473de45fd6bd6c31b6f9fc5
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2019-10-29 16:06:01 -0400

    Feature: Created the Addition function in Calculator.py

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,4 +1,15 @@
+def addition_of_two_numbers(num1, num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    return num1 + num2
+
+
 class Calculator:
+    result = 0
 
     def __init__(self):
         pass
+
+    def add(self, num1, num2):
+        self.result = addition_of_two_numbers(num1, num2)
+        return self.result
