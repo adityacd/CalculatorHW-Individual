f132c8470f1e443481a146a2a0aa4e3f0ca5ae34
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2019-10-30 16:12:13 -0400

    Feature: Created the division function and tested it

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,16 +1,21 @@
-def addition_of_two_numbers(a, b):
-    a = int(a)
-    b = int(b)
-    return a + b
+def addition_of_two_numbers(num1, num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    return num1 + num2
 
-def subtraction_of_two_number(a, b):
-    a = int(a)
-    b = int(b)
-    return b - a
+def subtraction_of_two_numbers(num1, num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    return num2 - num1
 
-def multiplication_of_two_number(num1, num2):
+def multiplication_of_two_numbers(num1, num2):
     c = num1 * num2
     return c
+
+def division_of_two_numbers(num1, num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    return round(num2 / num1, 7)
 
 class Calculator:
     result = 0
@@ -18,14 +23,18 @@ class Calculator:
     def __init__(self):
         pass
 
-    def add(self, a, b):
-        self.result = addition_of_two_numbers(a, b)
+    def add(self, num1, num2):
+        self.result = addition_of_two_numbers(num1, num2)
         return self.result
 
-    def subtract(self, a, b):
-        self.result = subtraction_of_two_number(a, b)
+    def subtract(self, num1, num2):
+        self.result = subtraction_of_two_numbers(num1, num2)
         return self.result
 
     def multiply(self, num1, num2):
-        self.result = multiplication_of_two_number(num1, num2)
+        self.result = multiplication_of_two_numbers(num1, num2)
+        return self.result
+
+    def divide(self, num1, num2):
+        self.result = round(division_of_two_numbers(num1, num2), 7)
         return self.result
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -15,20 +15,48 @@ class MyTestCase(unittest.TestCase):  # Accessing the methods
     def test_addition_method_calculator(self):
         test_data = CsvReader('./src/Unit Test Addition.csv').data
         for row in test_data:
-            self.assertEqual(self.calobject.add(row['Value 1'], row['Value 2']), int(row['Result']))
-            self.assertEqual(self.calobject.result, int(row['Result']))
+            x = row['Value 1']
+            # the second column in the text line is y value.
+            y = row['Value 2']
+            # the third column in the text line is (x + y) value.
+            expect_result = int(row['Result'])
+            self.assertEqual(self.calobject.add(x, y), expect_result)
+            self.assertEqual(self.calobject.result, expect_result)
 
     def test_subtraction_method_calculator(self):
         test_data = CsvReader('./src/Unit Test Subtraction.csv').data
         for row in test_data:
-            self.assertEqual(self.calobject.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
-            self.assertEqual(self.calobject.result, int(row['Result']))
+            x = row['Value 1']
+            # the second column in the text line is y value.
+            y = row['Value 2']
+            # the third column in the text line is (x + y) value.
+            expect_result = int(row['Result'])
+            self.assertEqual(self.calobject.subtract(x, y), expect_result)
+            self.assertEqual(self.calobject.result, expect_result)
+            #self.assertEqual(self.calobject.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
+            #self.assertEqual(self.calobject.result, int(row['Result']))
 
     def test_multiply_method_calculator(self):
         test_data = CsvReader('./src/Unit Test Multiplication.csv').data
         for row in test_data:
-            self.assertEqual(self.calobject.multiply(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
-            self.assertEqual(self.calobject.result, int(row['Result']))
+            x = int(row['Value 1'])
+            # the second column in the text line is y value.
+            y = int(row['Value 2'])
+            # the third column in the text line is (x + y) value.
+            expect_result = int(row['Result'])
+            self.assertEqual(self.calobject.multiply(x, y), expect_result)
+            self.assertEqual(self.calobject.result, expect_result)
+            #self.assertEqual(self.calobject.multiply(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
+            #self.assertEqual(self.calobject.result, int(row['Result']))
+
+    def test_divide_method_calculator(self):
+        test_data = CsvReader('./src/Unit Test Division.csv').data
+        for row in test_data:
+            x = float(row['Value 1'])
+            y = float(row['Value 2'])
+            z = float(row['Result'])
+            self.assertEqual(self.calobject.divide(x, y), round(z, 7))
+            self.assertEqual(self.calobject.result, round(z, 7))
 
 if __name__ == '__main__':  # This is just causing the unitttest to run
     unittest.main()
